7c507635dda2af603bc9b25c530e37796e4812a9
Suzowy <suzowy@gmail.com>
2024-04-03 12:29:52 +0200

    eliminada uri erronea

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ var app = require('./app');
 var PORT = process.env.PORT || 5000;
 var mongoUri = process.env.MONGO_DB_URI ;
 
-var mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://backend-demo:DMRyETF8CzjEF05rBxj8SIKCN2hyC1mQineQsqZVXbJMs5SKLfM1ameYPTkdrgC3GokSfROheZtdACDbHHp0LA==@backend-demo.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@backend-demo@", function (err, db) {
-  db.close();
-});
-
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
