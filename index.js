4acfad8bd8fddc3e653131c2d1a0f9c622ab9ec0
Suzowy <suzowy@gmail.com>
2024-04-23 11:05:08 +0200

    uri

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict';
-
 var mongoose = require('mongoose');
-var dotenv = require("dotenv")
+var app = require('./app');
+var dotenv = require('dotenv');
 dotenv.config();
 
-var app = require('./app');
-var PORT = process.env.PORT || 5000;
-var mongoUri = 'mongodb+srv://admin:admin@proyecto.ii5axio.mongodb.net/?retryWrites=true&w=majority&appName=proyecto';
-
-mongoose.Promise = global.Promise;
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
+// Conectar a MongoDB
+var mongoUri = process.env.MONGO_DB_URI;
+mongoose.connect(mongoUri)
+  .then(() => {
     console.log("Conexión establecida con éxito");
 
-    // Creación del servidor
-    app.listen(PORT, () => {
-        console.log(`Servidor en ejecución en el puerto: ${PORT}`);
+    // Iniciar el servidor
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Servidor en ejecución en el puerto: ${process.env.PORT || 5000}`);
     });
-})
-.catch(err => console.log(err));
+  })
+  .catch(err => {
+    console.error("Error al conectar a MongoDB:", err);
+    process.exit(1); // Salir del proceso si hay un error crítico
+  });
